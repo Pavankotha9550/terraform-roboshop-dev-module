325cf0b180501de5380fa915d48b84d78c51946d
pavankotha <pavankotha9550@outlook.com>
2026-02-28 11:45:12 +0530

    bastion server

---
+++ 20-bastion-server/data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "DevOps_practice_ami_id" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["973714476881"] 
+}
+
+data "aws_security_group" "bastion"{
+    name= bastion-sg_id
+}
---
+++ 20-bastion-server/ec2.tf
@@ -0,0 +1,10 @@
+
+resource "aws_instance" "bastion" {
+  ami           = data.DevOps_practice_ami_id.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids= data.aws_security_group.bastion.value
+
+  tags = {
+    Name="${var.project}-${var.environment}-bastion"
+  }
+}
