a11e0d507838710bf500946fbab1c29925c5fd3b
pavankotha <pavankotha9550@outlook.com>
2026-02-28 13:08:25 +0530

    bastion server

--- 20-bastion-server/ec2.tf
+++ 20-bastion-server/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.DevOps_practice_ami_id.id
   instance_type = "t3.micro"
-  vpc_security_group_ids= [data.aws_ssm_parameter.bastion.id]
+  vpc_security_group_ids= [data.aws_ssm_parameter.bastion.value]
 
   tags = {
     Name="${var.project}-${var.environment}-bastion"
