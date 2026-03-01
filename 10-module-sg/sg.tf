55b119420c00b1c3a1e7101035d1d2a53e719d17
pavankotha <pavankotha9550@outlook.com>
2026-03-01 21:14:24 +0530

    updated egress in sg

--- 10-module-sg/sg.tf
+++ 10-module-sg/sg.tf
@@ -3,8 +3,17 @@ resource "aws_security_group" "roboshop-sg" {
   description = var.description
   vpc_id      = var.vpc_id
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
   tags = {
     Name="${var.project}-${var.environment}-${var.server}"
     env=var.environment
   }
 }
+
