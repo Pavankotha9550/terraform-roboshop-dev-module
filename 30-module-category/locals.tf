30e29377d3474e1a3070fe03d26e17e37316c9c5
pavankotha <pavankotha9550@outlook.com>
2026-03-07 10:11:29 +0530

    updated

--- 30-module-category/locals.tf
+++ 30-module-category/locals.tf
@@ -3,4 +3,5 @@ locals {
     vpc_zone_identifier= var.component == "frontend" ? split("," ,data.aws_ssm_parameter.public_subnet_id.value) : split("," ,data.aws_ssm_parameter.private_subnet_id.value)
     listener_arn = var.component == "frontend" ? data.aws_ssm_parameter.flb-ARN.value : data.aws_ssm_parameter.alb-ARN.value
     port = var.component == "frontend" ? 80 : 8080
+    component= var.component
 }
