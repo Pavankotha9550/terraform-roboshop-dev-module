a80deb1d9932b264cc7ce1bee53da5894efc8c39
pavankotha <pavankotha9550@outlook.com>
2026-03-07 19:55:13 +0530

    updated

--- 30-module-category/ec2.tf
+++ 30-module-category/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
   ami           = data.aws_ami.DevOps_practice_ami_id.id
   instance_type = "t3.micro"
   vpc_security_group_ids= data.aws_ssm_parameter.sg_id.value
-  subnet_id= [local.subnet]
+  subnet_id= local.subnet
   #iam_instance_profile= "EC2RoleToFetchSSMParameter"
   
   
