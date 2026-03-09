e5cf81ec06a749c55fbc49827f25dee97e774136
Pavankotha9550 <pavankotha9550@outlook.com>
2026-03-09 15:26:07 +0530

    Update locals.tf

--- 30-module-category/locals.tf
+++ 30-module-category/locals.tf
@@ -5,6 +5,6 @@ locals {
     component= var.component
     #vpc_security_group_ids= data.aws_ssm_parameter.local.component.value
     #host = aws_instance.${local.component}.private_ip
-    listener_arns= var.component == "frontend" ? data.aws_ssm_parameter.flb-ARN-lisitner.value : data.aws_ssm_parameter.flb-ARN-lisitner.value
+    listener_arns= var.component == "frontend" ? data.aws_ssm_parameter.flb-ARN-lisitner.value : data.aws_ssm_parameter.alb-ARN-lisitner.value
     subnet = var.component == "frontend" ? split("," ,data.aws_ssm_parameter.public_subnet_id.value)[0] : split("," ,data.aws_ssm_parameter.private_subnet_id.value)[0]
 }
